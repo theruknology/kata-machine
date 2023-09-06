5238e09bd6157c8ce0f4c4b3e36e5d6cca6a98e4
Mohammed Ruknuddin <iosrukn123@gmail.com>
2023-09-06 10:44:44 +0300

    Quick

---
+++ src/day1/ArrayList.ts
@@ -0,0 +1,31 @@
+type Node<T> = {
+    
+}
+
+export default class ArrayList<T> {
+    public length: number;
+
+    
+
+    constructor() {
+    }
+
+    prepend(item: T): void {
+
+}
+    insertAt(item: T, idx: number): void {
+
+}
+    append(item: T): void {
+
+}
+    remove(item: T): T | undefined {
+
+}
+    get(idx: number): T | undefined {
+
+}
+    removeAt(idx: number): T | undefined {
+
+}
+}
