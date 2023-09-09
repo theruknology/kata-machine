7ffd6a93f42b0f644b35eb437dc4a9a13606b429
Mohammed Ruknuddin <iosrukn123@gmail.com>
2023-09-09 23:00:30 +0300

    Heaps

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,2 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
 }
--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,16 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+const search = (curr: BinaryNode<number> | null, needle: number): boolean => {
+  if (!curr) {
+    return false;
+  }
+  if (curr.value === needle) {
+    return true;
+  }
+  if (curr.value < needle) {
+    return search(curr.right, needle);
+  }
+  return search(curr.left, needle);
+}
 
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
